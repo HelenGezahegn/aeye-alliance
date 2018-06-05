a3ec44e5fd37b8f1b0b920774be75900c1e3e2bf
HelenG <gezahegn@ualberta.ca>
2018-06-05 17:08:13 -0400

    Mentor Help: Setting up a POST request to get the image uploaded

--- learn_flask.py
+++ learn_flask.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import time
+
 app = Flask(__name__)
- 
+
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+photos = UploadSet('photos', IMAGES)
+
+app.config['UPLOADED_PHOTOS_DEST'] = 'static'
+configure_uploads(app, photos)
+
 @app.route("/")
 def index():
     return render_template('home.html')
@@ -9,17 +17,18 @@ def index():
 def aboutus():
     return render_template('aboutus.html')
 
-# @app.route("/hello")
-# def hello():
-#     return "Hello World!"
- 
-# @app.route("/members")
-# def members():
-#     return "Members"
- 
-# @app.route("/members/<string:name>/")
-# def getMember(name):
-#     return name
+@app.route("/upload" )
+def upload():
+    return render_template('upload.html')
+
+@app.route("/save_img", methods=['GET', 'POST'])
+def save():
+    print(request.files)
+    print(request.method)
+    
+    if request.method == 'POST' and 'fileField' in request.files:
+        filename = photos.save(request.files['fileField'])
+    return render_template('display.html', filename=filename)
  
 if __name__ == "__main__":
     app.run(debug=True)
