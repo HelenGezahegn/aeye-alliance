8afa82e08d31ef6af95854a62279b317b1040580
Ting-Yi Su <tingyisu8@gmail.com>
2018-06-08 15:10:22 -0400

    current model (python & jupyter notebook files)

--- current_braille_to_text_converter.py
+++ current_braille_to_text_converter.py
@@ -1,235 +1,301 @@
+# all our import statements
+
 import time
-import platform
-import io
+# import platform
+# import io
+import random
+
 import matplotlib.pyplot as plt
-from matplotlib.pyplot import cm
+from matplotlib.pyplot import cm 
+
+# import torch
 import torch
 import torch.nn as nn
 import torch.optim as optim
+
+# import statements for csv dataset
 import torchvision
 import csv
 import os
-import pandas as pd
+import pandas as pd 
 from urllib import request
-from scipy import misc
-from io import BytesIO
-import urllib
-import numpy as np
-import tensorflow as tf
-import urllib.request
-from torchvision import transforms
-import string
+import requests
+
+# import statements for iterating over csv file
+# from scipy import misc
+# from io import BytesIO
+# import urllib
 import cv2
-from urllib import request
-from scipy import misc
-from io import BytesIO
-import urllib
+import numpy as np
+# import tensorflow as tf
 import urllib.request
-from torchvision import transforms
+# from torchvision import transforms
 
+# import to get the alphabet
+import string
 
+# Upload and read the csv files from the github repo
+df = pd.read_csv("https://raw.githubusercontent.com/HelenG123/aeye-alliance/master/Labelled%20Data/combined.csv")
 
+# generate the targets 
+# the targets are one hot encoding vectors
 
-# Upload and read the csv file from the github repo
-df = pd.read_csv("https://raw.githubusercontent.com/HelenG123/aeye-alliance/master/Data/data_day3.csv")
-df_test = pd.read_csv("https://raw.githubusercontent.com/HelenG123/ai-alliance/master/brailleFinalv2.csv")
-
-
-
-
-# using 0-25 to represent a-z instead
-target = {}
 alphabet = list(string.ascii_lowercase)
-number = 0 
+
+target = {}
+
+# Initalize a target dict that has the letters as its keys and as its value
+# an empty one-hot encoding of size 26
 for letter in alphabet: 
-  target[letter] = number
-  number += 1
+    target[letter] = [0] * 26
 
+# Do the one-hot encoding for each letter now 
+curr_pos = 0 
+for curr_letter in target.keys():
+    target[curr_letter][curr_pos] = 1
+    curr_pos += 1  
 
+print(target)
 
-
-# Iterate over the CSV files to add the targets
+# collect all data from the csv file
 data=[]
 
+# iterate over csv file
 for i, row in df.iterrows():
-  picture = []
-  url = row['Labeled Data']
-  label = row['Label']
-  curr_target = target[label[10]]
-  x = urllib.request.urlopen(url)
-  resp = x.read()
-  image = np.array(bytearray(resp), dtype=np.uint8)
-  image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-  # resize image to 28x28x3
-  image = cv2.resize(image, (28, 28))
-  image = image.astype(np.float32)/255.0
-  image = torch.from_numpy(image)
-  picture.append(image)
-  curr_target=torch.LongTensor([curr_target])
-  picture.append(curr_target)
-  data.append(picture)
+    # store the image and label
+    picture = []
+    url = row['Labeled Data']
+    label = row['Label']
+    curr_target = target[label[11]]
+    x = urllib.request.urlopen(url)
+    resp = x.read()
+    image = np.array(bytearray(resp), dtype=np.uint8)
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    # resize image to 28x28x3
+    image = cv2.resize(image, (28, 28))
+    # normalize to 0-1
+    image = image.astype(np.float32)/255.0
+    image = torch.from_numpy(image)
+    picture.append(image)
+    # convert the target to a long tensor
+    curr_target=torch.LongTensor([curr_target])
+    picture.append(curr_target)
+    # append the current image & target
+    data.append(picture)
 
-data_test=[]
+# Create dataloader objects
 
-for i, row in df_test.iterrows():
-  picture = []
-  url = row['Labeled Data']
-  label = row['External ID']
-  curr_target = target[label[0]]
-  x = urllib.request.urlopen(url)
-  resp = x.read()
-  image = np.array(bytearray(resp), dtype=np.uint8)
-  image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-  # resize image to 28x28x3
-  image = cv2.resize(image, (28, 28))
-  image = image.astype(np.float32)/255.0
-  image = torch.from_numpy(image)
-  picture.append(image)
-  curr_target=torch.LongTensor([curr_target])
-  picture.append(curr_target)
-  data_test.append(picture)
+# shuffle all the data
+random.shuffle(data)
 
-
-
-# Load the dataset
-batch_size = 10
+# batch sizes for train, test, and validation
+batch_size_train = 20
 batch_size_test = 5
+batch_size_validation = 5
 
-train_loader = torch.utils.data.DataLoader(dataset=data, batch_size=batch_size, shuffle=True)
-test_loader = torch.utils.data.DataLoader(dataset=data_test, batch_size=batch_size_test, shuffle=False)
+# splitting data to get training and validation sets
+# train has 1315 
+train_dataset = data[:1316]
+# test has 164
+test_dataset = data[1316:1479]
+# validation has 164
+validation_dataset = data[1479:]
 
-# import the nn.Module class
-import torch.nn as nn
-
+# create the dataloader objects
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size_train, shuffle=True)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size_test, shuffle=False)
+validation_loader = torch.utils.data.DataLoader(dataset=validation_dataset, batch_size=batch_size_validation, shuffle=True)
 
 # defines the convolutional neural network
+
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
         self.block1 = nn.Sequential(
-            # 3x28x28
-            nn.Conv2d(in_channels=3,
-                      out_channels=16,
-                      kernel_size=5,
-                      stride=1,
+            #3x28x28
+            nn.Conv2d(in_channels=3, 
+                      out_channels=16, 
+                      kernel_size=5, 
+                      stride=1, 
                       padding=2),
-            # 16x28x28
-            nn.MaxPool2d(kernel_size=2)
-            # 16x14x14
+            #16x28x28
+            nn.MaxPool2d(kernel_size=2),
+            #16x14x14
+            nn.LeakyReLU()
         )
-        # 16x14x14
+        #16x14x14
         self.block2 = nn.Sequential(
-            nn.Conv2d(in_channels=16,
-                      out_channels=32,
-                      kernel_size=5,
-                      stride=1,
+            nn.Conv2d(in_channels=16, 
+                      out_channels=32, 
+                      kernel_size=5, 
+                      stride=1, 
                       padding=2),
-            # 32x14x14
-            nn.MaxPool2d(kernel_size=2)
-            # 32x7x7
-        )
-        # linearly
+            #32x14x14
+            nn.MaxPool2d(kernel_size=2),
+            #32x7x7
+            nn.LeakyReLU()
+        ) 
+        # linearly 
         self.block3 = nn.Sequential(
-            nn.Linear(32 * 7 * 7, 500),
-            nn.Linear(500, 300),
-            nn.Linear(300, 100),
+            nn.Linear(32*7*7, 100),
+            nn.LeakyReLU(),
             nn.Linear(100, 26)
         )
-
-        # 1x26
-
-    def forward(self, x):
+        
+        #1x26
+    
+    def forward(self, x): 
         out = self.block1(x)
         out = self.block2(out)
-        # flatten the dataset=
-        out = out.view(-1, 32 * 7 * 7)
+        # flatten the dataset
+        out = out.view(-1, 32*7*7)
         out = self.block3(out)
-
+        
         return out
-
 
 # convolutional neural network model
 model = CNN()
 
-# Set the learning rate, criterion, & optimizer
+# print summary of the neural network model to check if everything is fine. 
+print(model)
+print("# parameter: ", sum([param.nelement() for param in model.parameters()]))
+
+#setting the learning rate
 learning_rate = 1e-3
+
+# Using a variable to store the cross entropy method
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
+
+# Using a variable to store the optimizer 
+optimizer = torch.optim.Adam(model.parameters(),lr = learning_rate)
 
 t0 = time.time()
 
-# variable to store the total loss
-total_loss = []
+# list of all train_losses in 30 epochs
+train_losses = []
+# list of all validation losses in 30 epochs
+validation_losses = []
+
+# total train losses
+total_train_losses = []
+# total validation losses
+total_validation_losses = []
 
 # for loop that iterates over all the epochs
 num_epochs = 10
 for epoch in range(num_epochs):
-
+    
     # variables to store/keep track of the loss and number of iterations
     train_loss = 0
-    num_iter = 0
+    num_iter_train = 0
 
     # train the model
     model.train()
-
-    # Iterate over data.
-    for i, (images, labels) in enumerate(train_loader):
-
-        # need to permute so that the images are of size 3x28x28
+    
+    # Iterate over train_loader
+    for i, (images, labels) in enumerate(train_loader):  
+        # need to permute so that the images are of size 3x28x28 
         # essential to be able to feed images into the model
         images = images.permute(0, 3, 1, 2)
 
         # Zero the gradient buffer
         # resets the gradient after each epoch so that the gradients don't add up
-        optimizer.zero_grad()
+        optimizer.zero_grad()  
 
         # Forward
         outputs = model(images)
 
-        # calculate the loss
-        loss = criterion(outputs, labels.view(-1))
-        print('loss:', loss)
-        total_loss.append(loss)
-        # Backward
+        # convert the labels from one hot encoding vectors into integer values 
+        labels = labels.view(-1, 26)
+        y_true = torch.argmax(labels, 1)
+
+        loss = criterion(outputs, y_true)
+
+        #print('loss:', loss)
+        total_train_losses.append(loss)
+        # Backward (computes all the gradients)
         loss.backward()
 
         # Optimize
-        # loops through all parameters and updates weights by using the gradients
+        # loops through all parameters and updates weights by using the gradients 
+        # takes steps backwards to optimize (to reach the minimum weight)
         optimizer.step()
         # update the training loss and number of iterations
         train_loss += loss.data[0]
-        num_iter += 1
+        num_iter_train += 1
 
-    print('Epoch: {}, Loss: {:.4f}'.format(
-        epoch + 1, train_loss / num_iter))
+    print('Epoch: {}'.format(epoch+1))
+    print('Training Loss: {:.4f}'.format(train_loss/num_iter_train))
+    # append the train_losses over all the epochs
+    train_losses.append(train_loss/num_iter_train)
+    
+    # variables to store/keep track of the loss and number of iterations
+    validation_loss = 0
+    num_iter_validation = 0
+    
+    # Iterate over train_loader
+    for i, (images, labels) in enumerate(validation_loader):  
+        # need to permute so that the images are of size 3x28x28 
+        # essential to be able to feed images into the model
+        images = images.permute(0, 3, 1, 2)
+        # print(images.shape)
 
+        # Zero the gradient buffer
+        # resets the gradient after each epoch so that the gradients don't add up
+        optimizer.zero_grad()  
+
+        # Forward
+        outputs = model(images)
+
+        # convert the labels from one hot encoding vectors to integer values
+        labels = labels.view(-1, 26)
+        y_true = torch.argmax(labels, 1)
+        # calculate the loss
+        loss = criterion(outputs, y_true)
+        #print('loss:', loss)
+        total_validation_losses.append(loss)
+
+        # update the training loss and number of iterations
+        validation_loss += loss.data[0]
+        num_iter_validation += 1
+
+    print('Validation Loss: {:.4f}'.format(validation_loss/num_iter_validation))
+    # append all validation_losses over all the epochs
+    validation_losses.append(validation_loss/num_iter_validation)
+    
     # evaluate the model
     model.eval()
 
     correct = 0
     total = 0
+    num_iter_test = 0
+    mean = 0
+    
+    # Iterate over test_loader
+    for images, labels in test_loader:  
 
-    # Iterate over data.
-    for images, labels in test_loader:
-
-        # need to permute so that the images are of size 3x28x28
+        # need to permute so that the images are of size 3x28x28 
         # essential to be able to feed images into the model
         images = images.permute(0, 3, 1, 2)
 
         # Forward
         outputs = model(images)
-        loss = criterion(outputs, labels.view(-1))
-        _, predicted = torch.max(outputs.data, 1)
 
-        # Statistics
-        total += labels.size(0)
-        correct += (predicted == labels).sum()
+        # convert the labels from one hot encoding vectors into integer values 
+        labels = labels.view(-1, 26)
+        y_true = torch.argmax(labels, 1)
 
-    print('Accuracy on the test set: {}%'.format(100 * correct / total))
+        # find the index of the prediction
+        y_pred = torch.argmax(outputs, 1).type('torch.FloatTensor')
+        # convert to FloatTensor
+        y_true = y_true.type('torch.FloatTensor')
 
+        # find the mean difference of the comparisons
+        mean += torch.mean(torch.eq(y_true, y_pred).type('torch.FloatTensor'))  
+
+    print('Accuracy on the test set: {:.4f}%'.format(mean/len(test_loader) * 100))
+
+# calculate time it took to train the model
 tf = time.time()
 print()
-print("time: {} s".format(tf - t0))
-
-torch.save(model.state_dict(), "model.pth")
-
+print("time: {} s" .format(tf-t0))
