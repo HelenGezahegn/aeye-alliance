da107392e9c74137f101be2142df90bae5f340ac
Wan-Chun Su <37888451+wanchunsu@users.noreply.github.com>
2018-06-19 23:49:20 -0400

    app with new screens
    app with new splashscreen and main screen

---
+++ Android App/AEyeAlliance(newScreens)/uCrop-master/sample/src/main/java/com/yalantis/ucrop/sample/AboutUs.java
@@ -0,0 +1,28 @@
+package com.yalantis.ucrop.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+
+public class AboutUs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+        TextView about_us =  (TextView) findViewById(R.id.about_us);
+        about_us.setMovementMethod(new ScrollingMovementMethod());
+
+        TextView link = (TextView) findViewById(R.id.source_code);
+        String linkText = "<a href='https://github.com/HelenG123/aeye-alliance'> Source Code </a>";
+        link.setText(Html.fromHtml(linkText));
+        link.setMovementMethod(LinkMovementMethod.getInstance());
+
+
+    }
+
+
+}
