401f3ee1f7e1904db2996aa8a4172fcb6f80d352
Wan-Chun Su <37888451+wanchunsu@users.noreply.github.com>
2018-06-21 15:06:31 -0400

    app

---
+++ Android App/AEyeAlliance App (with pics)/AEyeAlliance/sample/build/generated/source/buildConfig/activity/debug/com/yalantis/ucrop/sample/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.yalantis.ucrop.sample;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.yalantis.ucrop.sample";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "activity";
+  public static final int VERSION_CODE = 11;
+  public static final String VERSION_NAME = "1.2.3";
+  // Fields from product flavor: activity
+  public static final int RequestMode = 1;
+}
